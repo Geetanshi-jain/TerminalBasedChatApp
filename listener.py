04e3c900d0bb47c0d87e33a50ddaac55f890ec9e
Abhishek yaduwanshi <abhishekyaduwanshi4852@gmail.com>
2025-08-25 11:44:59 +0530

    listener add

--- listener.py
+++ listener.py
@@ -1,7 +1,7 @@
 from scapy.all import sniff, TCP, Raw, IP
 
-SERVER_IP = "192.168.55.165"   # आपका server ip
-SERVER_PORT = 12346            # आपका server port
+SERVER_IP = "192.168.55.165"  
+SERVER_PORT = 12346            
 
 def packet_callback(packet):
     if packet.haslayer(TCP) and packet.haslayer(Raw):
@@ -10,7 +10,7 @@ def packet_callback(packet):
 
         if tcp_layer.dport == SERVER_PORT or tcp_layer.sport == SERVER_PORT:
             print(f"\n[PACKET] {ip_layer.src} -> {ip_layer.dst}  |  {len(packet[Raw].load)} bytes")
-            print(f"Raw Data (encrypted): {packet[Raw].load[:100]}")  # सिर्फ first 100 bytes दिखाएंगे
+            print(f"Raw Data (encrypted): {packet[Raw].load[:100]}")  
 
 print(f"[INFO] Listening on TCP port {SERVER_PORT} ... (Ctrl+C to stop)")
 sniff(filter=f"tcp port {SERVER_PORT}", prn=packet_callback, store=False)
